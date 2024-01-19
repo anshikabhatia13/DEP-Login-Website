affa8a025d72729c802292608bbc8f1354505af2
anshikabhatia13 <96980105+anshikabhatia13@users.noreply.github.com>
2024-01-20 00:16:29 +0530

    frontend beautification

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,27 @@
 import { Link } from 'react-router-dom';
 import logo from '.././logo1.png';
+import logo2 from '../icon2.png'
 import './header.css'; 
 
 export default function Header({
   heading,
   paragraph,
   linkName,
-  linkUrl = "#"
+  linkUrl = "#",
+  afterLoginImageSrc,
+  afterLoginImageAlt
 }) {
   return (
     <div className="header-container flex flex-col items-center" >
-      <div className="header-logo flex justify-center">
+      <div className="header-logo flex justify-center"  >
         <img src={logo} alt="Logo" className="header-logo-img" />
+        {/* <img src={signuplogo} alt="Logo" className="header-logo-img" /> */}
       </div>
       <h2 className="header-heading mt-6 text-center text-3xl font-extrabold text-gray-900">
         {heading}
+        {afterLoginImageSrc && (
+        <img src={afterLoginImageSrc} alt={afterLoginImageAlt} className="after-login-image" />
+      )}
       </h2>
       <p className="header-paragraph mt-2 text-center text-sm text-gray-600 mt-5">
         {paragraph} {' '}
@@ -22,6 +29,7 @@ export default function Header({
           {linkName}
         </Link>
       </p>
+      
     </div>
   );
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,15 +1,19 @@
 import Header from "../components/Header";
 import Login from "../components/Login";
 import './login.css';
+import signuplogo from '../icon2.png'
 
 export default function LoginPage() {
     return (
         <div className="login-page-container">
             <Header
-                heading={<span style={{ fontWeight: 'bold', fontStyle: 'normal', fontSize: '2rem', color: '#333' }}>Login to Your Account</span>}
+                heading={<span style={{ fontWeight: 'bold', fontStyle: 'normal', fontSize: '1.5rem', color: '#43424c', marginBottom: '-20%' }}>Login to Your Account</span>}
                 paragraph="Don't have an account yet? "
                 linkName="Signup"
                 linkUrl="/signup"
+                afterLoginImageSrc={signuplogo}
+                afterLoginImageAlt="Signup Logo"
+                
             />
             <Login />
         </div>
